f1e8ff240776a9cb8129928e0c59fee1192934c2
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2019-04-24 20:55:13 +0300

    Init project with UI and currency

---
+++ js/main.js
@@ -0,0 +1,58 @@
+window.addEventListener("load", Init);
+
+function Init() {
+  let url = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
+  console.log("init");
+  Request(url, GetCurrency);
+}
+
+function Request(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.send();
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4) return;
+
+    if (xhr.status != 200) {
+      var errStatus = xhr.status;
+      var errText = xhr.statusText;
+      console.log(errStatus + ": " + errText);
+    } else {
+      var data = JSON.parse(xhr.responseText);
+      callback(data);
+    }
+  };
+}
+
+function GetCurrency(data) {
+  ///console.log("GetCurrency: ", data);
+
+  let currency = document.querySelector("#currency");
+
+ 
+
+  for (let i = 0; i < data.length; i++) {
+    let ccy = document.createElement("div");
+    ccy.className = "ccy";
+    ccy.innerHTML = data[i].ccy;
+    currency.appendChild(ccy);
+    let base_ccy = document.createElement("div");
+    base_ccy.className = "base_ccy";
+    base_ccy.innerHTML = data[i].base_ccy;
+    currency.appendChild(base_ccy);
+    let buy = document.createElement("div");
+    buy.className = "buy";
+    buy.innerHTML = data[i].buy;
+    currency.appendChild(buy);
+    let sale = document.createElement("div");
+    sale.className = "sale";
+    sale.innerHTML = data[i].sale;
+    currency.appendChild(sale);
+    //console.log(data[i].ccy, " ", data[i].base_ccy, " buy: ", data[i].buy, " sale: ", data[i].sale);
+  }
+}
+
+function GetNews(data) {
+  console.log("GetNews");
+}
